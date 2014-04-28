65d2df574a38253ab49b835793bfe49d790264d4
amybear <amy@hyperfine.com>
2014-04-27 23:42:39 -0700

    Added in diamond uibezierpath drawing capabilities

--- Matchismo/SetCardView.m
+++ Matchismo/SetCardView.m
@@ -25,16 +25,31 @@
     if (self.number == 1) {
         double yOfShape = y + (height/2.0) - (heightOfShape/2.0);
         CGRect boundsOfShapes = CGRectMake(xOfShape, yOfShape, widthOfShape, heightOfShape);
-        [self drawOvalWithBounds:boundsOfShapes withColor:self.color];
         
+        if (self.shape == SQUIGGLE) {
+            
+        } else if (self.shape == DIAMOND) {
+            [self drawDiamondWithBounds:boundsOfShapes withColor:self.color];
+        } else if (self.shape == OVAL) {
+            [self drawOvalWithBounds:boundsOfShapes withColor:self.color];
+        }
+
     } else if (self.number == 2) {
         double yOfShape1 = y + (height/2.0) - ((2.33*heightOfShape)/2.0);
         double yOfShape2 = yOfShape1 + (1.33*heightOfShape);
         
         CGRect boundsOfShapes1 = CGRectMake(xOfShape, yOfShape1, widthOfShape, heightOfShape);
         CGRect boundsOfShapes2 = CGRectMake(xOfShape, yOfShape2, widthOfShape, heightOfShape);
-        [self drawOvalWithBounds:boundsOfShapes1 withColor:self.color];
-        [self drawOvalWithBounds:boundsOfShapes2 withColor:self.color];
+
+        if (self.shape == SQUIGGLE) {
+            
+        } else if (self.shape == DIAMOND) {
+            [self drawDiamondWithBounds:boundsOfShapes1 withColor:self.color];
+            [self drawDiamondWithBounds:boundsOfShapes2 withColor:self.color];
+        } else if (self.shape == OVAL) {
+            [self drawOvalWithBounds:boundsOfShapes1 withColor:self.color];
+            [self drawOvalWithBounds:boundsOfShapes2 withColor:self.color];
+        }
         
     } else if (self.number == 3) {
         double yOfShape1 = (height/2.0) - ((3.67*heightOfShape)/2.0);
@@ -44,20 +59,41 @@
         CGRect boundsOfShapes1 = CGRectMake(xOfShape, yOfShape1, widthOfShape, heightOfShape);
         CGRect boundsOfShapes2 = CGRectMake(xOfShape, yOfShape2, widthOfShape, heightOfShape);
         CGRect boundsOfShapes3 = CGRectMake(xOfShape, yOfShape3, widthOfShape, heightOfShape);
-        [self drawOvalWithBounds:boundsOfShapes1 withColor:self.color];
-        [self drawOvalWithBounds:boundsOfShapes2 withColor:self.color];
-        [self drawOvalWithBounds:boundsOfShapes3 withColor:self.color];
+        
+        if (self.shape == SQUIGGLE) {
+            
+        } else if (self.shape == DIAMOND) {
+            [self drawDiamondWithBounds:boundsOfShapes1 withColor:self.color];
+            [self drawDiamondWithBounds:boundsOfShapes2 withColor:self.color];
+            [self drawDiamondWithBounds:boundsOfShapes3 withColor:self.color];
+        } else if (self.shape == OVAL) {
+            [self drawOvalWithBounds:boundsOfShapes1 withColor:self.color];
+            [self drawOvalWithBounds:boundsOfShapes2 withColor:self.color];
+            [self drawOvalWithBounds:boundsOfShapes3 withColor:self.color];
+        }
     }
 }
 
-- (UIBezierPath *)drawOvalWithBounds: (CGRect)bounds {
-    return [UIBezierPath bezierPathWithRoundedRect:bounds cornerRadius:10.0];
+- (void)drawOvalWithBounds: (CGRect)bounds withColor:(UIColor *)color {
+    UIBezierPath *oval = [UIBezierPath bezierPathWithRoundedRect:bounds cornerRadius:10.0];
+    [color setFill];
+    [oval fill];
 }
 
-- (void)drawOvalWithBounds: (CGRect)bounds withColor:(UIColor *)color {
-    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:bounds cornerRadius:10.0];
+- (void)drawDiamondWithBounds: (CGRect)bounds withColor:(UIColor *)color {
+    UIBezierPath *diamond = [[UIBezierPath alloc] init];
+    double x = bounds.origin.x;
+    double y = bounds.origin.y;
+    double width = bounds.size.width;
+    double height = bounds.size.height;
+    
+    [diamond moveToPoint:CGPointMake(x, y + height/2.0)];
+    [diamond addLineToPoint:CGPointMake(x + width/2, y)];
+    [diamond addLineToPoint:CGPointMake(x + width, y + height/2)];
+    [diamond addLineToPoint:CGPointMake(x + width/2, y + height)];
+    [diamond closePath];
     [color setFill];
-    [path fill];
+    [diamond fill];
 }
 
 //////////////////////////////////////
