0caecb3f811f950377e8162b5ea6bc07450c98df
amybear <amy@hyperfine.com>
2014-05-02 13:52:38 -0700

    Fixed some card redrawing logic

--- Matchismo/SetGameViewController.m
+++ Matchismo/SetGameViewController.m
@@ -72,7 +72,7 @@
 
 - (IBAction)getThreeMoreCards:(UIButton *)sender {
     [self.game drawMoreCards:3];
-    self.grid.minimumNumberOfCells = [self.game cardCount];
+    self.grid.minimumNumberOfCells = [self.cardViews count] + 3; // Changed
     for (SetCardView *scView in self.cardViews) {
         [scView removeFromSuperview];
     }
